0a8987132f781acf76584654d0aecf78630535f6
thinhthp <thinhthp1@gmail.com>
2025-07-03 12:43:17 +0700

    feat

--- zQuitSmoking.Repositories.ThinhTHP/Models/UserNotificationThinhThp.cs
+++ zQuitSmoking.Repositories.ThinhTHP/Models/UserNotificationThinhThp.cs
@@ -2,6 +2,7 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace zQuitSmoking.Repositories.ThinhTHP.Models;
 
@@ -9,18 +10,24 @@ public partial class UserNotificationThinhThp
 {
     public int UserNotificationThinhThpid { get; set; }
 
+    [Required(ErrorMessage = "User Account ID is required.")]
     public int UserAccountId { get; set; }
 
+    [Required(ErrorMessage = "Notification ID is required.")]
     public int NotificationThinhThpid { get; set; }
 
     public DateTime? SentDate { get; set; }
 
     public bool IsRead { get; set; }
 
+    [StringLength(500, ErrorMessage = "Response cannot exceed 500 characters.")]
     public string Response { get; set; }
 
+    [Required(ErrorMessage = "Status is required.")]
+    [StringLength(50, ErrorMessage = "Status cannot exceed 50 characters.")]
     public string Status { get; set; }
 
+    [Range(0, 10, ErrorMessage = "Attempt count must be between 0 and 10.")]
     public int? AttemptCount { get; set; }
 
     public DateTime? LastAttemptDate { get; set; }
